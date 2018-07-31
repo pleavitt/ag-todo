5f634eb08edda7ab087867ecc5d3a5f61131bad9
Paul Leavitt <pleavitt@gmail.com>
2018-07-31 19:23:51 +1000

    Updated Tomcat version to fix [] in request params

--- src/main/java/paul/leavitt/todo/Todo.java
+++ src/main/java/paul/leavitt/todo/Todo.java
@@ -20,8 +20,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class Todo implements Serializable {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private long id;
 
     @Size(min = 1, max = 50, message = "Must be between 1 and 50 chars long")
@@ -35,7 +34,7 @@ public class Todo implements Serializable {
     @CreatedDate
     private Date createdAt;
 
-    public Todo(String text) {
+    public Todo(String text){
         this.text = text;
     }
 
